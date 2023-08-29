7b4e5f72bd18cbf5a3f94b2dd9a9dc03e62ebcfc
兼续 <chenglin.mcl@antgroup.com>
2023-08-29 19:53:46 +0800

    :bug: fix: sortable list require id

--- src/ColorStudio/defaultConfig.ts
+++ src/ColorStudio/defaultConfig.ts
@@ -29,33 +29,38 @@ const defaultSystemConfig: ISystemConfig = {
 };
 
 const defaultColorList: ISchemaItem[] = [
-  { title: 'Primary', color: '#1677FF', darkColor: '#1677FF', type: 'normal' },
-  { title: 'Neutral', color: '#1677FF', darkColor: '#1677FF', type: 'neutral' },
+  { id: 'Primary', title: 'Primary', color: '#1677FF', darkColor: '#1677FF', type: 'normal' },
+  { id: 'Neutral', title: 'Neutral', color: '#1677FF', darkColor: '#1677FF', type: 'neutral' },
   {
+    id: 'Red',
     title: 'Red',
     color: '#da4a45',
     darkColor: '#da4a45',
     type: 'normal',
   },
   {
+    id: 'Yellow',
     title: 'Yellow',
     color: '#e09f00',
     darkColor: '#be8600',
     type: 'normal',
   },
   {
+    id: 'Green',
     title: 'Green',
     color: '#67c03a',
     darkColor: '#4ca41f',
     type: 'normal',
   },
   {
+    id: 'Sky',
     title: 'Sky',
     color: '#00bec3',
     darkColor: '#00a1a5',
     type: 'normal',
   },
   {
+    id: 'Purple',
     title: 'Purple',
     color: '#9b59eb',
     darkColor: '#9b59eb',
